797179a0e87d08fdf0a4e5ee01dec5c923978fa3
Adam Kohler <kohler@pdx.edu>
2018-06-11 11:25:25 -0700

    Update sha256_Compressor_CSA_Quasi_Pipeline.v

--- sha256_Compressor_CSA_Quasi_Pipeline.v
+++ sha256_Compressor_CSA_Quasi_Pipeline.v
@@ -1,14 +1,4 @@
 
-module CSA(x_in, y_in, z_in, s, c);
-	input reg [31:0] x_in,y_in,z_in;
-	output reg [31:0] s,c;
-            
-	for (i=0; i < 32; i=i +1) begin
-		fulladder inst (x_in[i], y_in[i], z_in[i], s[i], c[i]);
-	end	
-	
-endmodule
-
 module sha256_update_variables (
 		
 			           input  wire init_round, 
