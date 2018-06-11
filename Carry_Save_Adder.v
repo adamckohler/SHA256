edc6faf65f4ff529f2967ee28b9b39594f634a83
Adam Kohler <kohler@pdx.edu>
2018-06-11 12:00:31 -0700

    Update Carry_Save_Adder.v

--- Carry_Save_Adder.v
+++ Carry_Save_Adder.v
@@ -1,9 +1,22 @@
-module CSA(x_in, y_in, z_in, s, c);
-	input reg [31:0] x_in,y_in,z_in;
-	output reg [31:0] s,c;
+module CSA(x_in, y_in, z_in, s_out, c_out);
+	
+	input wire [31:0] x_in, 
+	input wire [31:0] y_in, 
+	input wire [31:0] z_in,
+	
+	output wire [31:0] s_out,
+	output wire [31:0] c_out
+	);
             
 	for (i=0; i < 32; i=i +1) begin
-		fulladder inst (x_in[i], y_in[i], z_in[i], s[i], c[i]);
+		fulladder inst (//Inputs to the adder
+				.a(x_in[i]), 
+				.b(y_in[i]), 
+				.c(z_in[i]), 
+				
+				//Outputs from the adder
+				.sum(s_out[i]), 
+				.carry(c_out[i])
+				);
 	end	
-	
 endmodule
